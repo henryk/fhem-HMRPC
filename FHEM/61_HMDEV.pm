9f761566f26307e3c5e9410ce2d4647a582119e9
Henryk Plötz <henryk@ploetzli.ch>
2015-09-20 19:32:02 +0200

    Add subdevice set handling (e.g. set HMDEV_KEQ12345 SETPOINT_2 20.0 will now do a setValue on HMDEV_KEQ12345:2)

--- FHEM/61_HMDEV.pm
+++ FHEM/61_HMDEV.pm
@@ -136,14 +136,26 @@ HMDEV_Set($@)
 	return "Unknown argument ? choose one of " if ($a[1] eq "?");
 
 	return "invalid set call @a" if(@a != 3 && @a != 4);
+
+	my $hmaddr = $hash->{hmaddr};
+
+	# If this is a main device and the set parameter contains a _sub postfix, rewrite this
+	# to be a non-postfix'ed set on the subdevice address
+	unless($hash->{hmaddr} =~ /:\d+$/) {
+		if($a[-2] =~ /(.*)_(\d+)$/) {
+			$hmaddr = $hmaddr . ":" . $2;
+			$a[-2] = $1;
+		}
+	}
+
 	# We delegate this call to the HMRPC IODev, after having added the device address
 	if(@a==4)
 	{
-		return HMRPC_Set($hash->{IODev},$hash->{IODev}->{NAME},$hash->{hmaddr},$a[1],$a[2],$a[3]);
+		return HMRPC_Set($hash->{IODev},$hash->{IODev}->{NAME},$hmaddr,$a[1],$a[2],$a[3]);
 	}
 	else
 	{
-		return HMRPC_Set($hash->{IODev},$hash->{IODev}->{NAME},$hash->{hmaddr},$a[1],$a[2]);
+		return HMRPC_Set($hash->{IODev},$hash->{IODev}->{NAME},$hmaddr,$a[1],$a[2]);
 	}
 }
 
